2ca51e2af15ea493995effbe680e1165ef24d896
Stephen Privitera <stephen.privitera@bayer.com>
2023-08-04 18:01:05 +0200

    minor edits

--- blog/style.py
+++ blog/style.py
@@ -1,7 +1,8 @@
 import seaborn as sns
 
 sns.set(
-    font_scale=1.5, 
+    font_scale=1.25, 
     rc={'figure.figsize':(8,6)}
 )
 sns.set_style("whitegrid")
+# sns.set_palette(sns.color_palette())
